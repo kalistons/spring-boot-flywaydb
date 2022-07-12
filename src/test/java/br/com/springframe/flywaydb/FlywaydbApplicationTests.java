ecd412a30f80efa645b128f001d732d5916a89c3
kaliston.sousa <kaliston.silvanio@gmail.com>
2022-07-12 00:58:50 -0300

    init

---
+++ src/test/java/br/com/springframe/flywaydb/FlywaydbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springframe.flywaydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywaydbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
